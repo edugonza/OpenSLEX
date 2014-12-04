0b2d442b514b5fbdb7092f0b74f361d14b19e702
Eduardo Gonzalez <edu.gonza.lopez@gmail.com>
2014-12-04 16:19:15 +0100

    Added MapDB XESLite package to export of SLEXLog

--- src/org/processmining/openslex/SLEXExporter.java
+++ src/org/processmining/openslex/SLEXExporter.java
@@ -3,18 +3,24 @@ package org.processmining.openslex;
 import java.util.Map.Entry;
 
 import org.deckfour.xes.classification.XEventAttributeClassifier;
+import org.deckfour.xes.extension.XExtension;
+import org.deckfour.xes.extension.XExtensionManager;
+import org.deckfour.xes.extension.std.XConceptExtension;
 import org.deckfour.xes.factory.XFactory;
 import org.deckfour.xes.factory.XFactoryRegistry;
+import org.deckfour.xes.model.XAttributeLiteral;
 import org.deckfour.xes.model.XAttributeMap;
 import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
 import org.deckfour.xes.model.impl.XAttributeLiteralImpl;
+import org.progressmining.xeslite.external.XFactoryExternalImpl;
 
 public class SLEXExporter {
 	
 	public static XLog exportPerspectiveToXLog(SLEXPerspective p) {
-		XFactory xfactory = XFactoryRegistry.instance().currentDefault();
+		//XFactory xfactory = XFactoryRegistry.instance().currentDefault();
+		XFactory xfactory = new XFactoryExternalImpl.MapDBDiskImpl();
 		XLog xlog = xfactory.createLog();
 		String[] classifierKeys = new String[] {"1:5:TABLE_NAME","1:6:OPERATION","1:1:COLUMN_CHANGES"};
 		xlog.getClassifiers().add(new XEventAttributeClassifier("Activity", classifierKeys));
@@ -25,8 +31,10 @@ public class SLEXExporter {
 		while ((t = trset.getNext()) != null) {
 			
 			XAttributeMap tAttrMap = xfactory.createAttributeMap();
-			tAttrMap.put("caseId", new XAttributeLiteralImpl("concept:name", t.getCaseId()));
-			tAttrMap.put("Id", new XAttributeLiteralImpl("Id", String.valueOf(t.getId())));
+			XAttributeLiteral caseIdAttr = xfactory.createAttributeLiteral("concept:name", t.getCaseId(), XConceptExtension.instance());
+			XAttributeLiteral idAttr = xfactory.createAttributeLiteral("Id", String.valueOf(t.getId()), null);
+			tAttrMap.put("caseId", caseIdAttr);
+			tAttrMap.put("Id", idAttr);
 			XTrace xt = xfactory.createTrace(tAttrMap);
 			
 			xlog.add(xt);
@@ -36,8 +44,9 @@ public class SLEXExporter {
 			
 			while ((e = erset.getNext()) != null) {
 				XAttributeMap eAttrMap = xfactory.createAttributeMap();
-				XEvent xe = xfactory.createEvent(eAttrMap);
-				eAttrMap.put("Id", new XAttributeLiteralImpl("Id", String.valueOf(e.getId())));
+				XEvent xe = xfactory.createEvent();
+				XAttributeLiteral eidAttr = xfactory.createAttributeLiteral("Id", String.valueOf(e.getId()), null);
+				eAttrMap.put("Id", eidAttr);
 				
 				for (Entry<SLEXAttribute,SLEXAttributeValue> entry: e.getAttributeValues().entrySet()) {
 					String keyStr = entry.getKey().toString();
@@ -48,9 +57,9 @@ public class SLEXExporter {
 					if (valStr == null) {
 						valStr = "";
 					}
-					eAttrMap.put(entry.getKey().toString(), new XAttributeLiteralImpl(keyStr,valStr));
+					eAttrMap.put(entry.getKey().toString(), xfactory.createAttributeLiteral(keyStr, valStr, null));
 				}
-				
+				xe.setAttributes(eAttrMap);
 				xt.add(xe);
 			}
 		}
