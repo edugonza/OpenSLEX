ed89395f1b21bb47d192009bd43349665c45727f
Eduardo Gonzalez <edu.gonza.lopez@gmail.com>
2016-05-03 17:50:24 +0200

    Fixed exporter to include timestamp in each event.

--- src/org/processmining/openslex/SLEXExporter.java
+++ src/org/processmining/openslex/SLEXExporter.java
@@ -3,13 +3,19 @@
  */
 package org.processmining.openslex;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map.Entry;
 
 import org.deckfour.xes.classification.XEventAttributeClassifier;
 import org.deckfour.xes.extension.std.XConceptExtension;
+import org.deckfour.xes.extension.std.XTimeExtension;
 import org.deckfour.xes.factory.XFactory;
 import org.deckfour.xes.model.XAttributeLiteral;
 import org.deckfour.xes.model.XAttributeMap;
+import org.deckfour.xes.model.XAttributeTimestamp;
 import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
@@ -42,6 +48,9 @@ public class SLEXExporter {
 			SLEXAttribute table_nameAttr = storageEvCol.findAttribute("COMMON", "TABLE_NAME");
 			SLEXAttribute operationAttr = storageEvCol.findAttribute("COMMON", "OPERATION");
 			SLEXAttribute column_changesAttr = storageEvCol.findAttribute("COMMON", "COLUMN_CHANGES");
+			SLEXAttribute timestampAttr = storageEvCol.findAttribute("COMMON", "TIMESTAMP");
+			DateFormat dformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S", Locale.ENGLISH);
+			
 			String[] classifier1Keys = new String[] {"_"+table_nameAttr.toString().replace(':','_'),"_"+operationAttr.toString().replace(':','_'),"_"+column_changesAttr.toString().replace(':','_')};
 			String[] classifier2Keys = new String[] {"_"+table_nameAttr.toString().replace(':','_'),"_"+operationAttr.toString().replace(':','_')};
 			xlog.getClassifiers().add(new XEventAttributeClassifier("Activity with changes vector", classifier1Keys));
@@ -87,6 +96,10 @@ public class SLEXExporter {
 						}
 						eAttrMap.put(keyStr, xfactory.createAttributeLiteral(keyStr, valStr, null));
 					}
+					SLEXAttributeValue timeVal = e.getAttributeValues().get(timestampAttr);
+					Date datetime = dformat.parse(timeVal.getValue());
+					XAttributeTimestamp time = xfactory.createAttributeTimestamp(XTimeExtension.KEY_TIMESTAMP,datetime,XTimeExtension.instance());
+					eAttrMap.put(time.getKey(), time);
 					xe.setAttributes(eAttrMap);
 					xt.add(xe);
 				}
